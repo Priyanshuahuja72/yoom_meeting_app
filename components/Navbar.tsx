a0b5db063dd13b2b75cc880be3fbd4b00f70ed9c
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2024-04-11 00:02:57 +0530

    final-changes-in-authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import MobileNav from './MobileNav'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import MobileNav from "./MobileNav";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
@@ -18,10 +19,13 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
+        <SignInButton>
+          <UserButton />
+        </SignInButton>
         <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
